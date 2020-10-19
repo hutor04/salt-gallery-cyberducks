af0723b810390fbb0d0cd0f459bc19e15a609ff1
Eugene Hutornuk <eugene@Salt-Admins-MacBook-Pro.local>
2020-10-19 13:40:31 +0200

    API done

--- server/api-route.js
+++ server/api-route.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const Unsplash = require('unsplash-js').default;
-const toJson = require('unsplash-js').toJson;
+const { toJson } = require('unsplash-js');
+const fetch = require('node-fetch');
+
+global.fetch = fetch;
 const config = require('../config');
+
 const router = express.Router();
 
 const unsplash = new Unsplash({ accessKey: config.unsplash.accesskey });
 
 router.get('/:query', (req, res) => {
   const pageNumber = req.query.p ? req.query.p : 1;
-  unsplash.search.photos(req.params.query, pageNumber, 10, { orientation: "portrait"})
-  .then(toJson)
-  .then(json => {
-    res.json(json);
-  });
+  const itemsPerPage = req.query.n ? req.query.n : 10;
+  unsplash.search.photos(req.params.query, pageNumber, itemsPerPage, { orientation: 'portrait' })
+    .then(toJson)
+    .then(json => {
+      const imageDataFiltered = json.results.map(result => {
+        return {
+          id: result.id,
+          description: result.description,
+          alt_description: result.alt_description,
+          urls: result.urls,
+          tags: result.tags
+        };
+      })
+      const filteredJson = {
+        total: json.total,
+        total_pages: json.total_pages,
+        results: imageDataFiltered
+      }
+      res.json(filteredJson);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ error: err.message });
+    });
 });
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const unsplash = require('./api-route');
+
 const app = express();
+
+const port = 3000;
+
 
 app.use(bodyParser.json());
 app.use('/api/unsplash', unsplash);
+app.use('/', express.static(path.join(__dirname, '../dist')));
+
 
 module.exports.app = app;
-if (!module.parent) { app.listen(3000); }
-console.log("Application started. Listening on port:" + port);
+if (!module.parent) { app.listen(port); }
